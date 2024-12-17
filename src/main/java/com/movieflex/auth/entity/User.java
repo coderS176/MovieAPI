880e383fccaba9beeb79da81193424405da2e592
Shivam Gupta <74005091+coderS176@users.noreply.github.com>
2024-12-17 21:08:50 +0530

    Added security to controller

--- src/main/java/com/movieflex/auth/entity/User.java
+++ src/main/java/com/movieflex/auth/entity/User.java
@@ -6,6 +6,7 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.security.core.GrantedAuthority;
@@ -19,6 +20,7 @@ import java.util.List;
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
+@Builder
 public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
